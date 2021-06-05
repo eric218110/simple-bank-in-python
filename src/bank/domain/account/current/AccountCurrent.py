ca7451c2bdd8ec0d74d383a873d6975654c82aeb
Eric Silva <ericsilvaccp@gmail.com>
2021-06-05 14:09:56 -0300

    feat: implements Account current

--- src/bank/domain/account/current/AccountCurrent.py
+++ src/bank/domain/account/current/AccountCurrent.py
@@ -1,2 +1,13 @@
 class AccountCurrent:
-    pass
+    def __init__(self, limitExtra: int) -> None:
+        self._limitExtra = limitExtra
+
+    @property
+    def limitExtra(self) -> int:
+        return self._limitExtra
+
+    @limitExtra.setter
+    def limitExtra(self, limitExtra: int) -> None:
+        if isinstance(limitExtra, int):
+            self._limitExtra = limitExtra
+        raise TypeError('Argument limitExtra not type int')
